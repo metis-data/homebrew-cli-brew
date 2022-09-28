d6581d22880c95310bd723e25e6f0938ab55f79f
gideonshmila <gideons@metisdata.io>
2022-09-28 17:44:32 +0300

    M

--- metis-cli-formula.rb
+++ metis-cli-formula.rb
@@ -3,7 +3,7 @@ class MetisCliFormula < Formula
   homepage ""
   url "https://github.com/metis-data/dba.ai/archive/refs/tags/dba-ai-repl@0.31.0.tar.gz"
   version "1.0.0"
-  sha256 "a7eff26da13d33660c60d97c1a213c5de25b60e92e3bf6a287b90faba5ec0a15"
+  sha256 "7eff26da13d33660c60d97c1a213c5de25b60e92e3bf6a287b90faba5ec0a15"
 
 
   def install
