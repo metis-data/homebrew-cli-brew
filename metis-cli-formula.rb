01a459e0ebeed53478ad9e8a9c8c82193563b176
gideonshmila <gideons@metisdata.io>
2022-09-28 16:48:49 +0300

    first-commit

---
+++ metis-cli-formula.rb
@@ -0,0 +1,13 @@
+class MetisCliBrewFormulaScript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/metis-data/dba.ai/archive/refs/tags/dba-ai-repl@0.31.0.tar.gz"
+  version "1.0.0"
+  sha256 "a7eff26da13d33660c60d97c1a213c5de25b60e92e3bf6a287b90faba5ec0a15"
+
+
+  def install
+     
+  end
+
+end
