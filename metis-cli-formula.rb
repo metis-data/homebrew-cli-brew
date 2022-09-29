dd642b595212e735484fd7e5ef6b9eba98ca198a
gideonshmila <gideons@metisdata.io>
2022-09-29 10:20:26 +0300

    m

--- metis-cli-formula.rb
+++ metis-cli-formula.rb
@@ -1,17 +1,13 @@
 class MetisCliFormula < Formula
   desc ""
   homepage ""
-  url "https://github.com/metis-data/homebrew-metis-cli-brew-formula/archive/refs/tags/v0.0.1.tar.gz"
+  url "https://github.com/metis-data/metis-cli/archive/refs/tags/0.31.0.tar.gz"
   version "1.0.0"
-  sha256 "f8f560be847e930369ddf7badb832994d8c28513d3fc71de43f28061837f3160"
+  sha256 "ec3cd50c911cdaa769b3922a41e434acd60e06dee08942030da6a55e1015723f"
 
-  
+
   def install
-    require 'net/http'
-    require 'uri'
-
-    uri = URI.parse("https://github.com/metis-data/dba.ai/archive/refs/tags/dba-ai-repl@0.31.0.tar.gz")
-    response = Net::HTTP.get_response(uri)
+    
   end
 
 end
