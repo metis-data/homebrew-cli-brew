f40dfea9746e16d3ebdab811996b7d75de7821e5
gideonshmila <gideons@metisdata.io>
2022-10-03 19:16:55 +0300

    m

--- metis-cli.rb
+++ metis-cli.rb
@@ -1,7 +1,7 @@
 class MetisCli < Formula
   desc ""
   homepage ""
-  url "https://github.com/metis-data/metis-cli/releases/download/0.32.0/metis-macos-0.32.0"
+  url "https://github.com/metis-data/metis-cli/releases/download/0.32.1/metis_macos_0.32.1"
   version "0.32.0"
   sha256 "26abd41badaace542ba51fb0b9d025d643ba33be7c048e52eb81564fc4a0b99f"
 
@@ -9,7 +9,7 @@ class MetisCli < Formula
   def install
     require "fileutils"
 
-    File.rename("metis-macos-0.32.0", "metis-cli")
+    File.rename("metis-macos-0.32.1", "metis-cli")
     FileUtils.chmod("+x", "metis-cli")
     bin.install "metis-cli"
   end
