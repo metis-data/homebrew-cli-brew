8c486aa43a783a1189f91596b437384bf6d10f19
gideonshmila <gideons@metisdata.io>
2022-10-13 16:49:55 +0300

    update sha256

--- metis-cli.rb
+++ metis-cli.rb
@@ -3,7 +3,7 @@ class MetisCli < Formula
   homepage ""
   url "https://github.com/metis-data/metis-cli/releases/download/0.32.1/metis_linux_0.32.2"
   version "1.0.0"
-  sha256 "0618418a9f2901569141b0edaa9ec74df25fcec27f72de3cc82e78ac63e9c5d3"
+  sha256 "04dcd12a66f76d83b1c163c4675b323109082facd9cbabbc5f034f44227b7331"
 
   
   def install
