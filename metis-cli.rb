5ee64c043b4bd310667f4c6e4379f409b5408c85
gideonshmila <gideons@metisdata.io>
2022-10-26 14:05:14 +0300

    M-porgess

--- metis-cli.rb
+++ metis-cli.rb
@@ -1,7 +1,7 @@
 class MetisCli < Formula
   desc ""
   homepage ""
-  url "https://github.com/metis-data/metis-cli/releases/download/0.4.1/metis_macos_0.4.1.2"
+  url "https://github.com/metis-data/metis-cli/releases/download/0.4.2/metis_macos_0.4.2"
   version "1.0.0"
   sha256 "663694771f7532855c24dbd04e1841c0d32886efc0601f9d1ec81eeadad84c1f"
   
